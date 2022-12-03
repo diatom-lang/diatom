5464a056d31f0de348ef3d5c03e700738f674fdc
TerenceNg03 <stoicism03@gmail.com>
2022-12-03 23:32:29 +0800

    Console now may interpret file

--- src/bin/diatom.rs
+++ src/bin/diatom.rs
@@ -1,6 +1,26 @@
-use diatom::Console;
+use diatom::{Console, Parser};
+use std::path::PathBuf;
+
+use clap::Parser as ArgParser;
+
+#[derive(ArgParser)]
+#[command(version)]
+#[command(about = "The diatom interpreter and compiler")]
+struct Args {
+    /// Path to target file
+    file: Option<PathBuf>,
+}
 
 fn main() {
-    let mut console = Console::new();
-    console.run();
+    let args = Args::parse();
+
+    if let Some(path) = args.file {
+        let mut parser = Parser::new();
+        parser.parse(path.as_os_str());
+        parser.print_diagnoses();
+        parser.print_diagnoses_summary();
+    } else {
+        let mut console = Console::new();
+        console.run();
+    }
 }
